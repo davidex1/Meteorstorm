ea5bccda9ac6c368b0ddb25a4f6a656bb5e89d15
davidex1 <dawid.switalski100@wp.pl>
2017-11-30 23:56:58 +0100

    Added manifest and some cleaning

--- js/playground.js
+++ js/playground.js
@@ -1,6 +1,104 @@
-window.onload = function () {
-    var x = document.getElementById("exit");
-    x.onclick = function () {
-        document.innerHTML = Date();
+function start() {
+    movement();
+    fallLoop();
+}
+
+window.onload = start;
+
+function movement() {
+    
+    // defining basic elements of the game
+    var ship = document.getElementById("ship");
+    var ground = document.getElementById("playground");
+    
+    // scripts for ship movement
+    ground.addEventListener("mousemove", shipMove);
+    ground.addEventListener("touchmove", shipMoveTouch);
+    
+    function shipMove(ev) {
+        
+        var R = window.innerWidth;
+        var borderL = 0;
+        var borderR = R - 100;
+        
+        var x = ev.clientX - 50;
+        
+        if (x > borderL && x < borderR) {
+            ship.style.left = x + "px";
+        } 
+    }
+    
+    function shipMoveTouch(ev) {
+        
+        var R = window.innerWidth;
+        var borderL = 50;
+        var borderR = R - 50;
+        ev.preventDefault();
+        var touch = ev.targetTouches[0];
+        var x = touch.clientX - 50;
+        
+        if (touch.clientX > borderL && touch.clientX < borderR) {
+            ship.style.left = x + "px";
         }
     }
+}
+
+function fallLoop() {
+        
+    var meteors = [
+    document.getElementById("meteor1"),
+    document.getElementById("meteor2"),
+    document.getElementById("meteor3"),
+    document.getElementById("meteor4"),
+    document.getElementById("meteor5"),
+    document.getElementById("meteor6")];
+
+    function fall() {
+        var randMeteor = meteors[Math.floor(Math.random()*meteors.length)];
+        var pos = 0;
+        var id = setInterval(frame, 0.5);
+        
+        function frame() {
+            if (pos == 1000) {
+                clearInterval(id);
+            } 
+            else {
+                pos++; 
+                randMeteor.style.top = pos + 'px';  
+            }
+        }
+    }
+        
+    setInterval(fall, 5000);
+    
+    }
+
+// positions of ship and meteors
+/*    var shipL = ship.offsetLeft;
+    var shipR = shipL + 100;
+    var shipT = ship.offsetTop;
+    
+    var meteorL = meteors[2].offsetLeft;
+    var meteorT = meteors[2].offsetTop;*/
+
+
+// script for falling meteors loop
+    /*ground.addEventListener("touchstart", fallLoop);
+    
+    var shipPos = ship.getBoundingClientRect();
+    var meteor1Pos = meteors[0].getBoundingClientRect();
+    var meteor2Pos = meteors[1].getBoundingClientRect();
+    var meteor3Pos = meteors[2].getBoundingClientRect();
+    var meteor4Pos = meteors[3].getBoundingClientRect();
+    var meteor5Pos = meteors[4].getBoundingClientRect();
+    var meteor6Pos = meteors[5].getBoundingClientRect();
+    
+    
+    function fallLoop() {
+        while (meteor1Pos.top != shipPos.top && meteor1Pos.left != shipPos.left) {
+                fall();
+                if (meteor1Pos.top = shipPos.top && meteor1Pos.left != shipPos.left) {
+                    break;
+                }
+            }
+        } */                       
